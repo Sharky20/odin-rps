f4d7930ec65caf60c74cef4d5456fc86f9e05014
Colby Mullins <42915868+Sharky20@users.noreply.github.com>
2025-05-09 15:49:22 -0400

    Add playRound function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     if (choice == 0) {
@@ -13,4 +16,47 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     return prompt("choice: ");
+}
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase()
+    if (humanChoice == "paper") {
+        if (computerChoice == "paper") {
+            console.log("Tie!");
+        }
+        else if (computerChoice == "rock") {
+            console.log("You win!");
+            humanScore++;
+        }
+        else {
+            console.log("You lost!");
+            computerScore++;
+        }
+    }
+    else if (humanChoice == "rock") {
+        if (computerChoice == "rock") {
+            console.log("Tie!");
+        }
+        else if (computerChoice == "scissors") {
+            console.log("You win!");
+            humanScore++;
+        }
+        else {
+            console.log("You lost!");
+            computerScore++;
+        }
+    }
+    else {
+        if (computerChoice == "scissors") {
+            console.log("Tie!");
+        }
+        else if (computerChoice == "paper") {
+            console.log("You win!");
+            humanScore++;
+        }
+        else {
+            console.log("You lost!");
+            computerScore++;
+        }
+    }
 }

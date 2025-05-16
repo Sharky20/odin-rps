34265165bf7868eda07b4b05c01876726b2a38d4
Colby Mullins <42915868+Sharky20@users.noreply.github.com>
2025-05-16 14:13:54 -0400

    Add rock paper scissors buttons.
    Add buttons with each choice and implement event listeners.
    Game is played in console when buttons are clicked.

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let humanScore = 0;
 let computerScore = 0;
 
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
+
+rockBtn.addEventListener("click", (e) => playRound("rock", getComputerChoice()));
+paperBtn.addEventListener("click", (e) => playRound("paper", getComputerChoice()));
+scissorsBtn.addEventListener("click", (e) => playRound("scissors", getComputerChoice()));
+
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     if (choice == 0) {
